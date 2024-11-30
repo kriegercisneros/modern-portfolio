2ba604e5c3df2e32632d2d63cadef328ca3116de
JackieCisneros <krieger.jacqueline@gmail.com>
2024-11-30 16:39:00 -0700

    initial

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'John Doe - Portfolio',
+  title: 'Jackie Krieger - Portfolio',
   description: 'Full Stack Developer Portfolio',
 }
 
