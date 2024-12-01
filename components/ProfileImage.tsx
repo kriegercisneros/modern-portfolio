cf73a88b81b46ec653d4eba615ac2e99ea1159dd
Jacqueline Krieger-Cisneros <111556905+kriegercisneros@users.noreply.github.com>
2024-11-30 17:27:59 -0700

    Fix ProfileImage sizing and positioning

--- components/ProfileImage.tsx
+++ components/ProfileImage.tsx
@@ -3,14 +3,14 @@ import profileImage from '../public/images/DSC_6152.jpg'
 
 export default function ProfileImage() {
   return (
-    <div className="relative w-32 h-32 mx-auto rounded-full overflow-hidden"> {/* Changed from w-64 h-64 to w-32 h-32 */}
+    <div className="relative w-24 h-24"> {/* Container with fixed size */}
       <Image
         src={profileImage}
         alt="Profile Picture"
-        fill
-        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+        width={96}    // Specify exact width
+        height={96}   // Specify exact height
+        className="rounded-full object-cover"
         priority
-        className="object-cover"
         quality={90}
       />
     </div>
