eb9e39c58f852f5ad15af5af390d43b5e761e2e1
Jacqueline Krieger-Cisneros <111556905+kriegercisneros@users.noreply.github.com>
2024-11-30 17:24:50 -0700

    Update ProfileImage component to be smaller

--- components/ProfileImage.tsx
+++ components/ProfileImage.tsx
@@ -3,15 +3,15 @@ import profileImage from '../public/images/DSC_6152.jpg'
 
 export default function ProfileImage() {
   return (
-    <div className="relative w-64 h-64 mx-auto rounded-full overflow-hidden">
+    <div className="relative w-32 h-32 mx-auto rounded-full overflow-hidden"> {/* Changed from w-64 h-64 to w-32 h-32 */}
       <Image
-        src={profileImage}  // Update this with your image filename
+        src={profileImage}
         alt="Profile Picture"
         fill
         sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-        priority  // Use this if the image is above the fold
+        priority
         className="object-cover"
-        quality={90}  // Adjust quality (default is 75)
+        quality={90}
       />
     </div>
   )
