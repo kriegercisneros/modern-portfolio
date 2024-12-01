65ae033a7f8ee251bb37b95b8fa54d49a22c13ce
Jacqueline Krieger-Cisneros <111556905+kriegercisneros@users.noreply.github.com>
2024-11-30 18:22:23 -0700

    fix: update ProfileImage to display as perfect circle

--- components/ProfileImage.tsx
+++ components/ProfileImage.tsx
@@ -2,24 +2,23 @@ import Image from 'next/image'
 import profileImage from '../public/images/DSC_6152.jpg'
 
 export default function ProfileImage() {
-  const imageSize = 128; // 32 * 4 for better quality on retina displays
+  const imageSize = 128;
 
   return (
     <div className="flex justify-center items-center">
-      <div className="relative w-64"> {/* Force GPU acceleration */}
-        <div className="w-full rounded-full overflow-hidden">
+      <div className="relative w-32 h-32">
+        <div className="w-full h-full rounded-full overflow-hidden">
           <Image
             src={profileImage}
             alt="Profile Picture"
             width={imageSize}
-            className="object-cover w-full"
+            height={imageSize}
+            className="object-cover"
             quality={85}
             priority
-            unoptimized
           />
         </div>
       </div>
     </div>
-    
   )
 }
